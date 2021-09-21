6cba5a8c4eb7215985025b9f6d27d5c6c59bee22
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2021-09-21 21:45:08 +0500

    workshop finished

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -6,17 +6,57 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
+import { render as utilRender } from '../../test/test-utils';
+
+
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+  // const customRender = (ui, options) =>
+  // render(ui, {wrapper: ThemeProvider, ...options})
+
+  render(<ThemeProvider initialTheme="light"><EasyButton>Easy</EasyButton></ThemeProvider>)
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
+})
+test('renders with the dark styles for the dark theme', () => {
+  render(<ThemeProvider initialTheme="dark"><EasyButton>Easy</EasyButton></ThemeProvider>)
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
+})
+
+const customRender = (ui, theme, options) =>
+  render(ui, {wrapper: (props)=> <ThemeProvider initialTheme={theme} {...props} />, ...options})
+
+test('renders with the light styles for the light theme', () => {
+		customRender(<EasyButton>Easy</EasyButton>, 'light')
+		const button = screen.getByRole('button', {name: /easy/i})
+		expect(button).toHaveStyle(`
+			background-color: white;
+			color: black;
+		`)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+	customRender(<EasyButton>Easy</EasyButton>, 'dark')
+	const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+	utilRender(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+	const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
 })
 
 /* eslint no-unused-vars:0 */
--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,21 +2,71 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
+import {renderHook, act as hookAct } from '@testing-library/react-hooks'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
+
+const CountComponent = () => {
+	const {count, increment, decrement} = useCounter();
+	return <div>
+		<span>count is {count}</span>
+		<button onClick={increment}>increment</button>
+		<button onClick={decrement}>decrement</button>
+	</div>
+}
 
 test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+	render(<CountComponent />)
+	screen.debug();
+	const increaseButton = screen.getByText(/increment/i), decreaseButton = screen.getByText(/decrement/i), result = screen.getByText(/count is/i)
+	expect(result.textContent).toEqual('count is 0')
+	userEvent.click(increaseButton)
+	expect(result.textContent).toEqual('count is 1')
+	userEvent.click(decreaseButton)
+	expect(result.textContent).toEqual('count is 0')
 })
 
+test('exposes the count and increment/decrement functions', () => {
+	let counter
+	function TestComponent(props) {
+		counter = useCounter()
+		return null
+	}
+	render(<TestComponent />)
+	expect(counter.count).toEqual(0)
+	act(()=> counter.increment());
+	expect(counter.count).toEqual(1)
+	act(()=> counter.decrement());
+	expect(counter.count).toEqual(0)
+})
+
+
+test('exposes the count and increment/decrement functions', () => {
+	function setup(useCustomHook, ...args) {
+		const returnVal = {}
+		function TestComponent() {
+			Object.assign(returnVal, useCustomHook(...args))
+			return null
+		}
+		render(<TestComponent />)
+		return returnVal
+	}
+	const counter = setup(useCounter, {initialCount:1, step:3} )
+	expect(counter.count).toEqual(1)
+	act(()=> counter.increment());
+	expect(counter.count).toEqual(4)
+	act(()=> counter.decrement());
+	expect(counter.count).toEqual(1)
+})
+
+test('exposes the count and increment/decrement functions', () => {
+	const { result } = renderHook(() => useCounter({initialCount:1, step:3}));
+	expect(result.current.count).toEqual(1)
+	hookAct(()=> result.current.increment());
+	expect(result.current.count).toEqual(4)
+	hookAct(()=> result.current.decrement());
+	expect(result.current.count).toEqual(1)
+})
 /* eslint no-unused-vars:0 */
